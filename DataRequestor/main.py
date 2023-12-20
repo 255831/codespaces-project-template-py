c89e31162e7636764261d4a94b738c70236c9362
255831 <79965941+255831@users.noreply.github.com>
2023-12-20 09:57:57 +0000

    20.12

--- DataRequestor/main.py
+++ DataRequestor/main.py
@@ -2,17 +2,31 @@ from time import sleep
 #from snap7.server import mainloop
 import requests
 
-res = requests.get("https://api.openaq.org/v2/measurements?location_id=9342&parameter=no2&parameter=co&parameter=pm25&date_from=2023-11-07T10:55:08+01:00&date_to=2023-11-08T10:55:08+01:00&limit=1000").json() #wisniowa
-res2 = requests.get("https://api.openaq.org/v2/measurements?location_id=10566&parameter=so2&parameter=pm10&parameter=co&parameter=no2&parameter=bc&parameter=pm25&parameter=o3&date_from=2023-11-07T09:58:56+01:00&date_to=2023-11-08T09:58:56+01:00&limit=1000").json() #conrada
-
-
+class WeatherRequester:
+    def __init__(self, myx):
+        self.x = myx
+        self.dic = {
+            "limit": 100,
+            "page": 1,
+            "offset": 0,
+            "sort":"desc",
+            "radius":1000,
+            "city":self.x,
+            "order_by":"lastUpdated",
+            "dump_raw":False}
+    def __str__(self):
+        return f" lokalizacja to {self.myx}"
+    def get_data(self):
+        res = requests.get("https://api.openaq.org/v2/locations", params=self.dic).json()
+        print(res['results'][0])
 
 while (True):
-    #print('Test')
-    print(res2['results'][0]['location'])
-    sleep(2)
-    #print (res.header)
-    sleep(2)
+    s  = input("podaj lokalizacje: ")
+    p = WeatherRequester(s)
+    print( "Wybrano", p.x)
+    p.get_data()
+    sleep(30)
+
 
 
 
